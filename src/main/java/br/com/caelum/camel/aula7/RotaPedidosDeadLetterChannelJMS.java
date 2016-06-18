d3fd0b962fe4920fe723d925bc28be31d24f6477
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2016-06-18 16:34:27 -0300

    ADD: exercícios da aula 7

--- src/main/java/br/com/caelum/camel/aula7/RotaPedidosDeadLetterChannelJMS.java
+++ src/main/java/br/com/caelum/camel/aula7/RotaPedidosDeadLetterChannelJMS.java
@@ -1,38 +1,41 @@
-package br.com.caelum.camel.aula6;
+package br.com.caelum.camel.aula7;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class RotaPedidosOnException {
+public class RotaPedidosDeadLetterChannelJMS {
 
     public static void main(String[] args) throws Exception {
 
         CamelContext context = new DefaultCamelContext();
-        context.addRoutes(new RouteBuilder() {
 
+        context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616/"));
+
+        context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
 
-                onException(Exception.class)
-                        .handled(true)
-                        .maximumRedeliveries(3)
-                        .redeliveryDelay(4000)
-                        .onRedelivery(exchange -> {
-                            int counter = (int)
-                                    exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
-                            int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
-                            System.out.println("Redelivery - " + counter + "/" + max);
-                        });
+                errorHandler(deadLetterChannel("activemq:queue:pedidos.DLQ")
+                                     .logExhaustedMessageHistory(true)
+                                     .useOriginalMessage()
+                                     .maximumRedeliveries(3)
+                                     .maximumRedeliveryDelay(1000).onRedelivery(exchange -> {
+                            int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+                            int maxCounter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+                            System.out.println("REDELIVERY " + counter + " de " + maxCounter + " !!!!");
+                        }));
 
-                from("file:pedidos?delay=5s&noop=true")
+                from("activemq:queue:pedidos")
                         .routeId("rota-pedidos")
+                        .to("validator:pedido.xsd")
                         .multicast()
-                        .to("validator:pedido.xsd");
-//                        .to("direct:soap")
-//                        .to("direct:http");
+                        .to("direct:soap")
+                        .to("direct:http");
+//                    .to("validator:pedido.xsd")
 
                 from("direct:http")
                         .routeId("rota-http")
@@ -44,16 +47,14 @@ public class RotaPedidosOnException {
                         .xpath("/item/formato[text()='EBOOK']")
                         .setProperty("ebookId", xpath("/item/livro/codigo/text()"))
                         .marshal().xmljson()
-                        .log("${id} - ${body}")
                         .setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
                         .setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))
                         .to("http4://localhost:8080/webservices/ebook/item");
 
-                //inserindo a utilização do xslt!!!
                 from("direct:soap")
                         .routeId("rota-soap")
                         .to("xslt:pedido-para-soap.xslt")
-                        .log("Resultado do Template: ${body}")
+                        .log("${body}")
                         .setHeader(Exchange.CONTENT_TYPE, constant("text/xml"))
                         .to("http4://localhost:8080/webservices/financeiro");
             }
